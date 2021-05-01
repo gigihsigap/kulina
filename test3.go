674aeba7ba1ef5f8fca9c5a71dcaa1e6e5780055
gigihsigap <gigihsigap@gmail.com>
2021-05-01 11:03:45 +0700

    Submit files

--- test3.go
+++ test3.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"strconv"
 )
 
 func main() {
-	// conversion(1345679)
-	fmt.println("lol")
+	conversion(1345679)
 }
 
-// func conversion(num) {
-// 	fmt.println("lol")
-// }
+func conversion(num int) {
+
+	str := strconv.Itoa(num)
+
+	for i := 0; i < len(str); i++ {
+		zeros := int(math.Pow(10, float64(len(str)-i-1)))
+		number, _ := strconv.Atoi(string(str[i]))
+		result := number * zeros
+		fmt.Println(result)
+	}
+}
